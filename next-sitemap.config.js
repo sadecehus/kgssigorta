1d231d381b7772fd471e12ebb4ac877858095e4f
sadecehus <kocaturk4145@gmail.com>
2025-07-24 11:31:37 +0300

    google indexing

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.kgssigorta.com', // kendi domaininle değiştir
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'weekly',
+  priority: 0.7,
+  trailingSlash: true,
+};
